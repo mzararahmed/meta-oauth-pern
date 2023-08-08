436e6ce37aa2d97a403e92583e6626d02719dc67
mzararahmed <md.zarar.ahmed@gmail.com>
2023-08-08 11:52:20 +0500

    packages added for fb-login, cors, axios

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
+var cors = require('cors')
 const app = express();
 const port = 3000;
 
 const db = require('./queries');
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
