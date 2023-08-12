53c228d6b50c9e0bf85f5ba3ea9f6a772b2caf42
mzararahmed <md.zarar.ahmed@gmail.com>
2023-08-12 17:22:09 +0500

    client build serving index.html from server

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-require('dotenv').config();
-var cors = require('cors');
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+var cors = require("cors");
 const app = express();
 const port = process.env.PORT;
+const db = require("./queries");
 
-const db = require('./queries');
-
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
+app.use(express.static("client/build"));
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
-app.get('/users', db.getUsers);
-app.get('/users/:id', db.getUserById);
-app.post('/users', db.createUser);
-app.put('/users/:id', db.updateUser);
-app.delete('/users/:id', db.deleteUser);
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
