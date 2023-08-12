cd32ea9a11002cf0d2e8a278f0b802b8ea6fab6d
mzararahmed <md.zarar.ahmed@gmail.com>
2023-08-12 20:42:13 +0500

    user and social login components added

---
+++ client/src/SocialLogin.js
@@ -0,0 +1,246 @@
+import React, { useCallback, useState } from 'react';
+import './App.css';
+import { User } from './User';
+import {
+  LoginSocialGoogle,
+  LoginSocialAmazon,
+  LoginSocialFacebook,
+  LoginSocialGithub,
+  LoginSocialInstagram,
+  LoginSocialLinkedin,
+  LoginSocialMicrosoft,
+  LoginSocialPinterest,
+  LoginSocialTwitter,
+  LoginSocialApple
+} from 'reactjs-social-login';
+
+import {
+  FacebookLoginButton,
+  GoogleLoginButton,
+  GithubLoginButton,
+  AmazonLoginButton,
+  InstagramLoginButton,
+  LinkedInLoginButton,
+  MicrosoftLoginButton,
+  TwitterLoginButton,
+  AppleLoginButton,
+} from 'react-social-login-buttons';
+
+// @ts-ignore
+import { ReactComponent as PinterestLogo } from './assets/pinterest.svg';
+
+const REDIRECT_URI =
+  'https://plenty-planets-beam-42-118-51-2.loca.lt/account/login';
+// const REDIRECT_URI = 'http://localhost:3000/account/login'
+
+const App = () => {
+  const [provider, setProvider] = useState('');
+  const [profile, setProfile] = useState();
+
+  const onLoginStart = useCallback(() => {
+    alert('login start');
+  }, []);
+
+  const onLogoutSuccess = useCallback(() => {
+    // @ts-ignore
+    setProfile(null);
+    setProvider('');
+    alert('logout success');
+  }, []);
+
+  const onLogout = useCallback(() => {}, []);
+
+  return (
+    <>
+      {provider && profile && (
+        // @ts-ignore
+        <User provider={provider} profile={profile} onLogout={onLogout} />
+      )}
+      <div className={`App ${provider && profile ? 'hide' : ''}`}>
+        <h1 className="title">ReactJS Social Login</h1>
+        <LoginSocialFacebook
+          appId={process.env.REACT_APP_FACEBOOK_APP_ID || ''}
+          fieldsProfile={
+            'id,first_name,last_name,middle_name,name,name_format,picture,short_name,email,gender'
+          }
+          onLoginStart={onLoginStart}
+          onLogoutSuccess={onLogoutSuccess}
+          redirect_uri={REDIRECT_URI}
+          onResolve={({ provider, data }) => {
+            setProvider(provider);
+            // @ts-ignore
+            setProfile(data);
+          }}
+          onReject={err => {
+            console.log(err);
+          }}
+        >
+          <FacebookLoginButton />
+        </LoginSocialFacebook>
+
+        <LoginSocialGoogle
+          client_id={process.env.REACT_APP_GG_APP_ID || ''}
+          onLoginStart={onLoginStart}
+          redirect_uri={REDIRECT_URI}
+          scope="openid profile email"
+          discoveryDocs="claims_supported"
+          access_type="offline"
+          onResolve={({ provider, data }) => {
+            setProvider(provider);
+            // @ts-ignore
+            setProfile(data);
+          }}
+          onReject={err => {
+            console.log(err);
+          }}
+        >
+          <GoogleLoginButton />
+        </LoginSocialGoogle>
+
+        <LoginSocialApple
+          client_id={process.env.REACT_APP_APPLE_ID || ''}
+          scope={'name email'}
+          redirect_uri={REDIRECT_URI}
+          onLoginStart={onLoginStart}
+          onResolve={({ provider, data }) => {
+            setProvider(provider);
+            // @ts-ignore
+            setProfile(data);
+          }}
+          onReject={err => {
+            console.log(err);
+          }}
+        >
+          <AppleLoginButton />
+        </LoginSocialApple>
+
+        <LoginSocialAmazon
+          client_id={process.env.REACT_APP_AMAZON_APP_ID || ''}
+          redirect_uri={REDIRECT_URI}
+          onResolve={({ provider, data }) => {
+            setProvider(provider);
+            // @ts-ignore
+            setProfile(data);
+          }}
+          onReject={(err) => {
+            console.log(err);
+          }}
+          onLoginStart={onLoginStart}
+        >
+          <AmazonLoginButton />
+        </LoginSocialAmazon>
+
+        <LoginSocialInstagram
+          client_id={process.env.REACT_APP_INSTAGRAM_APP_ID || ''}
+          client_secret={process.env.REACT_APP_INSTAGRAM_APP_SECRET || ''}
+          redirect_uri={REDIRECT_URI}
+          onLoginStart={onLoginStart}
+          onLogoutSuccess={onLogoutSuccess}
+          onResolve={({ provider, data }) => {
+            setProvider(provider);
+            // @ts-ignore
+            setProfile(data);
+          }}
+          onReject={(err) => {
+            console.log(err);
+          }}
+        >
+          <InstagramLoginButton />
+        </LoginSocialInstagram>
+
+        <LoginSocialMicrosoft
+          client_id={process.env.REACT_APP_MICROSOFT_APP_ID || ''}
+          redirect_uri={REDIRECT_URI}
+          onLoginStart={onLoginStart}
+          onResolve={({ provider, data }) => {
+            setProvider(provider);
+            // @ts-ignore
+            setProfile(data);
+          }}
+          onReject={(err) => {
+            console.log(err);
+          }}
+        >
+          <MicrosoftLoginButton />
+        </LoginSocialMicrosoft>
+
+        <LoginSocialLinkedin
+          client_id={process.env.REACT_APP_LINKEDIN_APP_ID || ''}
+          client_secret={process.env.REACT_APP_LINKEDIN_APP_SECRET || ''}
+          redirect_uri={REDIRECT_URI}
+          onLoginStart={onLoginStart}
+          onResolve={({ provider, data }) => {
+            setProvider(provider);
+            // @ts-ignore
+            setProfile(data);
+          }}
+          onReject={(err) => {
+            console.log(err);
+          }}
+        >
+          <LinkedInLoginButton />
+        </LoginSocialLinkedin>
+
+        <LoginSocialGithub
+          client_id={process.env.REACT_APP_GITHUB_APP_ID || ''}
+          client_secret={process.env.REACT_APP_GITHUB_APP_SECRET || ''}
+          redirect_uri={REDIRECT_URI}
+          onLoginStart={onLoginStart}
+          onLogoutSuccess={onLogoutSuccess}
+          onResolve={({ provider, data }) => {
+            setProvider(provider);
+            // @ts-ignore
+            setProfile(data);
+          }}
+          onReject={(err) => {
+            console.log(err);
+          }}
+        >
+          <GithubLoginButton />
+        </LoginSocialGithub>
+        <LoginSocialPinterest
+          client_id={process.env.REACT_APP_PINTEREST_APP_ID || ''}
+          client_secret={process.env.REACT_APP_PINTEREST_APP_SECRET || ''}
+          redirect_uri={REDIRECT_URI}
+          onLoginStart={onLoginStart}
+          onResolve={({ provider, data }) => {
+            setProvider(provider);
+            // @ts-ignore
+            setProfile(data);
+          }}
+          onReject={(err) => {
+            console.log(err);
+          }}
+          className="pinterest-btn"
+        >
+          <div className="content">
+            <div className="icon">
+              <PinterestLogo />
+            </div>
+            <span className="txt">Login With Pinterest</span>
+          </div>
+        </LoginSocialPinterest>
+
+        <LoginSocialTwitter
+          client_id={process.env.REACT_APP_TWITTER_V2_APP_KEY || ''}
+          // client_secret={process.env.REACT_APP_TWITTER_V2_APP_SECRET || ''}
+          redirect_uri={REDIRECT_URI}
+          onLoginStart={onLoginStart}
+          onLogoutSuccess={onLogoutSuccess}
+          onResolve={({ provider, data }) => {
+            setProvider(provider);
+            // @ts-ignore
+            setProfile(data);
+          }}
+          onReject={(err) => {
+            console.log(err);
+          }}
+        >
+          <TwitterLoginButton />
+        </LoginSocialTwitter>
+      </div>
+    </>
+  );
+};
+
+export default App;
