600de57c316fa4ec2988b57304e9330707aa1338
mzararahmed <md.zarar.ahmed@gmail.com>
2023-08-16 20:12:56 +0500

    UI and styling changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import FacebookLogin from "react-facebook-login";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import { FaFacebook } from "react-icons/fa";
 import React from "react";
 import axios from "axios";
 import "./App.css";
 
 const App = () => {
   const [fbResponse, setFbResponse] = React.useState([]);
-
   const responseFacebook = async (response) => {
     if (response.status === "unknown") {
       // @ts-ignore
@@ -31,15 +31,34 @@ const App = () => {
   return (
     <>
       {fbResponse.length === 0 && (
-        <div className="loginBtn">
+        <div>
           <FacebookLogin
             // @ts-ignore
             appId={process.env.REACT_APP_FACEBOOK_APP_ID}
             autoLoad={false}
-            fields="name,email,picture"
+            fields="name,picture"
             callback={responseFacebook}
             scope="pages_show_list,pages_messaging"
-            icon="fa-facebook"
+            render={(renderProps) => (
+              <div className="container d-flex justify-content-center align-items-center">
+                <div className="card">
+                  <div className="card-header">
+                    <h2 className="card-title">Login with Facebook</h2>
+                  </div>
+                  <div className="card-body">
+                    <button
+                      className="btn btn-facebook"
+                      onClick={renderProps.onClick}
+                    >
+                      <FaFacebook className="mr-2" />
+                      <span className="font-weight-bold-i">
+                        Continue with Facebook
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
           />
         </div>
       )}
@@ -47,89 +66,58 @@ const App = () => {
         {fbResponse &&
           fbResponse.map((response) => {
             return (
-              <>
-                <div>
-                  <div className="container mt-4 mb-4 p-3 d-flex justify-content-center">
-                    <div className="card p-4">
-                      <div className="image d-flex flex-column justify-content-center align-items-center">
-                        <button className="btn btn-secondary">
-                          <img
-                            // @ts-ignore
-                            src={response.profilepictureurl}
-                            height="50"
-                            width="50"
-                            alt="profile"
-                          />
-                        </button>
-                        <span className="name mt-3">
+              <div
+                key={
+                  // @ts-ignore
+                  response.userid
+                }
+              >
+                <div className="container">
+                  <div className="card">
+                    <div className="card-header">
+                      <h2 className="card-title">User Information</h2>
+                    </div>
+                    <div className="card-body user-info">
+                      <img
+                        // @ts-ignore
+                        src={response.profilepictureurl}
+                        alt="User Profile"
+                        className="img-thumbnail rounded-circle"
+                      />
+                      <p>
+                        Name:{" "}
+                        {
+                          // @ts-ignore
+                          response.name
+                        }
+                      </p>
+                      <p>
+                        User ID:{" "}
+                        {
+                          // @ts-ignore
+                          response.userid
+                        }
+                      </p>
+                      <div className="d-flex flex-column align-items-center">
+                        <p className="w-30">
+                          Access Token:{" "}
                           {
                             // @ts-ignore
-                            response.name
+                            response.accesstoken
                           }
-                        </span>
-                        <span className="idd">@code.zedd</span>
-                        <div className="d-flex flex-row justify-content-center align-items-center gap-2">
-                          <span className="idd1">
-                            {
-                              // @ts-ignore
-                              response.userid
-                            }
-                          </span>
-                          <span>
-                            <i className="fa fa-copy"></i>
-                          </span>
-                        </div>
-                        <div className="d-flex flex-row justify-content-center align-items-center mt-3">
-                          <span className="number">
-                            1069<span className="follow">Followers</span>
-                          </span>
-                        </div>
-                        <div className=" d-flex mt-2">
-                          <button className="btn1 btn-dark">
-                            Edit Profile
-                          </button>
-                        </div>
-                        <div className="text mt-3">
-                          <span>
-                            Access Token:
-                            <p style={{ wordWrap: "break-word" }}>
-                              {
-                                // @ts-ignore
-                                response.accesstoken
-                              }
-                            </p>
-                            <br></br> Pages Token:
-                            <p>
-                              {
-                                // @ts-ignore
-                                response.pagestoken
-                              }
-                            </p>
-                            {}
-                          </span>
-                        </div>
-                        <div className="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">
-                          <span>
-                            <i className="fa fa-twitter"></i>
-                          </span>{" "}
-                          <span>
-                            <i className="fa fa-facebook-f"></i>
-                          </span>{" "}
-                          <span>
-                            <i className="fa fa-instagram"></i>
-                          </span>{" "}
-                          <span>
-                            <i className="fa fa-linkedin"></i>
-                          </span>
-                        </div>
-                        <div className=" px-2 rounded mt-4 date ">
-                          <span className="join">Joined May,2017</span>
-                        </div>
+                        </p>
+                        <p className="w-30">
+                          Pages Token:{" "}
+                          {
+                            // @ts-ignore
+                            response.pagestoken
+                          }
+                        </p>
                       </div>
                     </div>
                   </div>
                 </div>
-              </>
+              </div>
             );
           })}
       </div>
