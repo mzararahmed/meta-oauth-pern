dc357d910c46b6893cea48ac8629a7d0d7d81825
mzararahmed <md.zarar.ahmed@gmail.com>
2023-08-25 22:34:08 +0500

    query changes

--- queries.js
+++ queries.js
@@ -1,109 +1,73 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.HEROKU_POSTGRESQL_ROSE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
-})
-client.connect(function(err) {
+    rejectUnauthorized: false,
+  },
+});
+client.connect(function (err) {
   if (err) throw err;
   console.log("Connected to Heroku PostgresQL DB!");
 });
 
-client.query("CREATE TABLE IF NOT EXISTS users(name VARCHAR, userid VARCHAR, accesstoken VARCHAR, pagestoken VARCHAR, profilepictureurl VARCHAR)");
-
-
-const getUsers = (request, response) => {
-  client.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(results.rows);
-  });
-};
-
-const getUserById = (request, response) => {
-  const id = parseInt(request.params.id);
-
-  client.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(results.rows);
-  });
-};
+client.query(
+  "CREATE TABLE IF NOT EXISTS users(name VARCHAR, userid VARCHAR, accesstoken VARCHAR, profilepictureurl VARCHAR, pages text[])");
 
 const createUser = (request, response) => {
-  const { name, userId, accessToken, pagestoken, profilepictureurl } = request.body;
-  try{
-    client.query('SELECT * FROM users WHERE userId = $1', [userId], (error, results) => {
-      if(error || !results.rowCount){
-        console.error(error)
-        client.query(
-          'INSERT INTO users (name, userid, accesstoken, pagestoken, profilepictureurl) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-          [name, userId, accessToken, pagestoken, profilepictureurl],
-          (error, results) => {
-            if (error) {
-              throw error;
-            }
-            response.status(201).json(results.rows);
-          }
-        );
-      }
-
-      else {
-        if(accessToken !== results.rows[0].accesstoken) {
+  const { name, userId, accessToken, profilepictureurl, pages } =
+    request.body;
+  try {
+    client.query(
+      "SELECT * FROM users WHERE userId = $1",
+      [userId],
+      (error, results) => {
+        if (error || !results.rowCount) {
+          console.error(error);
           client.query(
-            'UPDATE users SET accessToken = $1, pagestoken = $2 WHERE userid = $3 RETURNING *',
-            [accessToken, pagestoken, userId],
+            "INSERT INTO users (name, userid, accesstoken, profilepictureurl, pages) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+            [name, userId, accessToken, profilepictureurl, pages],
             (error, results) => {
               if (error) {
                 throw error;
               }
-              response.status(200).json(results.rows);
+              response.status(201).json(results.rows);
             }
           );
+        } else {
+          if (accessToken !== results.rows[0].accesstoken) {
+            client.query(
+              "UPDATE users SET accessToken = $1, pages = $2 WHERE userid = $3 RETURNING *",
+              [accessToken, pages, userId],
+              (error, results) => {
+                if (error) {
+                  throw error;
+                }
+                response.status(200).json(results.rows);
+              }
+            );
+          }
         }
       }
-    })
+    );
+  } catch (error) {
+    console.error(error);
   }
-  catch(error){
-    console.error(error)
-  }
-};
-
-const updateUser = (request, response) => {
-  const id = parseInt(request.params.id);
-  const { name, email } = request.body;
-
-  client.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-    [name, email, id],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      response.status(200).send(`User modified with ID: ${id}`);
-    }
-  );
 };
 
 const deleteUser = (request, response) => {
-  const userId = parseInt(request.params.id);
+  const { userid } = request.body;
 
-  client.query('DELETE FROM users WHERE userId = $1', [userId], (error, results) => {
+  client.query('DELETE FROM users WHERE userid = $1', [userid], (error, results) => {
     if (error) {
       throw error;
     }
-    response.status(200).send(`User deleted with ID: ${userId}`);
+    console.log(userid)
+    response.status(200).send(`User deleted with ID: ${userid}`);
   });
 };
 
 module.exports = {
-  getUsers,
-  getUserById,
   createUser,
-  updateUser,
-  deleteUser,
+  deleteUser
 };
