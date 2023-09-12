c0c424bf74673807aed133c584e8487158eaebfa
mzararahmed <zararworkahmed@gmail.com>
2023-09-12 15:43:53 +0500

    structure changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-// import SocialLogin from "./SocialLogin";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import App from './App';
+import './index.css';
 
 // @ts-ignore
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
